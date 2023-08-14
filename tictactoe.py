2c8de37ff6205d4b2fafc7ad9c340438f69b7191
Sean <seanerfan@gmail.com>
2023-08-14 16:23:16 -0400

    All TicTacToes are in the same module

--- tictactoe.py
+++ tictactoe.py
@@ -104,7 +104,7 @@ class TicTacToe:
             return tuple(ret)
         ret[move] = self.turn
         return tuple(ret)
-        
+
 
 class TicTacToe2D(TicTacToe):
     # sets up the game
@@ -154,44 +154,6 @@ class TicTacToe2D(TicTacToe):
         return ret
 
 
-if __name__ == "__main__":
-    input("When prompted to enter a move, enter an integer. For example, to place in (1, 2), enter '12'.")
-    game = TicTacToe2D()
-
-    while True:
-        myStr = input("Enter a move: ")
-
-        if myStr.lower() == "reset":
-            game.reset()
-            print("Game has been reset.")
-            continue
-        
-        try:
-            myInt = int(myStr)
-        except ValueError:
-            print("Invalid move, enter a valid integer")
-            continue
-        
-        x = myInt // 10
-        y = myInt % 10
-        
-        if (x not in range(0, game.dimSize)) or (y not in range(0, game.dimSize)):
-            print("Move is out of bounds, try again")
-            continue
-
-        game.place((x, y))
-
-        if game.spotTaken == False:
-            print(game)
-
-        if game.gameOver == True:
-            print(f"Player '{game.players[game.turn+1]}' wins!")
-            break
-
-        if game.remainingTurns == 0:
-            print("Draw")
-            break
-
 class TicTacToe3D(TicTacToe):
     # sets up the game
     # 
@@ -202,6 +164,7 @@ class TicTacToe3D(TicTacToe):
     # - board: The board
     # - numPlayers: Number of players playing
     # - turn: Whose turn it is (indexed at zero)
+    # TODO: implement getPossibleActions, add 3D diagonals 
     def __init__(self) -> None:
         super().__init__(dims=3, dimSize=4, players=['', 'x', 'o'])
 
@@ -246,3 +209,77 @@ class TicTacToe3D(TicTacToe):
         del xSlice
 
         return False
+
+def test2D():
+    input("When prompted to enter a move, enter an integer. For example, to place in (1, 2), enter '12'.")
+    game = TicTacToe2D()
+
+    while True:
+        myStr = input("Enter a move: ")
+
+        if myStr.lower() == "reset":
+            game.reset()
+            print("Game has been reset.")
+            continue
+        
+        try:
+            myInt = int(myStr)
+        except ValueError:
+            print("Invalid move, enter a valid integer")
+            continue
+        
+        x = myInt // 10
+        y = myInt % 10
+        
+        if (x not in range(0, game.dimSize)) or (y not in range(0, game.dimSize)):
+            print("Move is out of bounds, try again")
+            continue
+
+        game.place((x, y))
+
+        if game.spotTaken == False:
+            print(game)
+
+        if game.gameOver == True:
+            print(f"Player '{game.players[game.turn+1]}' wins!")
+            break
+
+        if game.remainingTurns == 0:
+            print("Draw")
+            break
+
+def test3D():
+    input("When prompted to enter a move, enter an integer. For example, to place in (1, 2, 1), enter '121'.")
+    game = TicTacToe3D()
+
+    while True:
+        myStr = input("Enter a move: ")
+        try:
+            myInt = int(myStr)
+        except ValueError:
+            print("Invalid move, enter a valid integer")
+            continue
+
+        x = math.floor(myInt / 100)
+        y = math.floor(myInt / 10) % 10
+        z = math.floor(myInt % 10)
+
+        if (x not in range(0, game.dimSize)) or (y not in range(0, game.dimSize)) or (z not in range(0, game.dimSize)):
+            print("Move is out of bounds, try again")
+            continue
+    
+        game.place((x, y, z))
+
+        if game.spotTaken == False:
+            print(game)
+
+        if game.gameOver == True:
+            print(f"Player '{game.players[game.turn+1]}' wins!")
+            break
+
+        if game.remainingTurns == 0:
+            print("Draw")
+            break
+
+if __name__ == "__main__":
+    test3D()
