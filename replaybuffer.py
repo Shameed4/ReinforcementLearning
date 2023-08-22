581f79d3531bad5d484d2e29c06d4053fcf490e5
Mattzh0 <110125986+Mattzh0@users.noreply.github.com>
2023-08-22 15:57:53 -0400

    Replay Buffer update

---
+++ replaybuffer.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.memory=[]
+        self.capacity=capacity
+
+    def getMemory(self):
+        return self.memory
+
+    def getCapacity(self):
+        return self.capacity
+
+    def add(self, state, action, reward, nextState, done):
+        if len(self.memory) > self.capacity:
+            return
+        experience = (self, state, action, reward, nextState,done)
+        self.memory.append(experience)
+
+    def sample(self, batch_size):
+        pass
+
